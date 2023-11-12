e744ef308cabad25b5d34032f654ec59baee7673
anthonysuarez <antsuarez305@gmail.com>
2023-11-12 10:33:28 -0600

    Create document list item

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,9 +1,10 @@
 'use client'
 
+import { ChevronDown, ChevronRight, LucideIcon } from 'lucide-react'
+
 import { Id } from '@/convex/_generated/dataModel'
 import { cn } from '@/lib/utils'
-import { ChevronDown, ChevronRight, LucideIcon } from 'lucide-react'
-import { act } from 'react-dom/test-utils'
+import { Skeleton } from '@/components/ui/skeleton'
 
 interface ItemProps {
   id?: Id<'documents'>
@@ -30,6 +31,13 @@ export const Item = ({
   onExpand,
   expanded,
 }: ItemProps) => {
+  const handleExpand = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    onExpand?.()
+  }
+
   const ChevronIcon = expanded ? ChevronDown : ChevronRight
 
   return (
@@ -46,7 +54,7 @@ export const Item = ({
         <div
           role='button'
           className='h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1'
-          onClick={() => {}}
+          onClick={handleExpand}
         >
           <ChevronIcon className='h-4 w-4 shrink-0 text-muted-foreground/50' />
         </div>
@@ -62,6 +70,20 @@ export const Item = ({
           <span className='text-xs'>⌘</span>K
         </kbd>
       )}
+    </div>
+  )
+}
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${level * 12 + 25}px` : '12px',
+      }}
+      className='flex gap-x-2 py-[3px]'
+    >
+      <Skeleton className='h-4 w-4' />
+      <Skeleton className='h-4 w-[30%]' />
     </div>
   )
 }
